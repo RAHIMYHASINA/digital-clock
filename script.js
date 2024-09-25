eeab214bebfe49f0c3302d87d8035410fa0ec3c0
Hasina Rahimy <hasinarahimy5656@gmail.com>
2024-09-26 01:15:28 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+function changeTheme(theme) {
+    document.getElementById('clock-container').className = theme;
+}
+
+setInterval(updateClock, 1000);
+
